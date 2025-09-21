6724b1b10a53b561ac249ea80780d466b13ac211
Suimei Yakagi <24110107@student.hcmute.edu.vn>
2025-09-21 12:43:41 +0700

    up lên

---
+++ source/src/UI/GraphVisualizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace src.UI
+{
+    public partial class GraphVisualizer : Form
+    {
+        public GraphVisualizer()
+        {
+            InitializeComponent();
+        }
+    }
+}
